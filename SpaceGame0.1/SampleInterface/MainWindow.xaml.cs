89c9ab80acc3d6670d006119d25afbb5c18800b3
vnci <vbahovski@gmail.com>
2014-02-10 00:40:04 +0200

    Демо на двупосочен преход. Все още малко разхвърляно, но вече не е никакъв проблем.

--- SpaceGame0.1/SampleInterface/MainWindow.xaml.cs
+++ SpaceGame0.1/SampleInterface/MainWindow.xaml.cs
@@ -26,38 +26,16 @@ namespace SampleInterface
             InitializeComponent();
         }
 
-        /// <summary>
-        /// Finds a path, a number of levels above the current one, so that another directory in the same solution can be accessed
-        /// </summary>
-        /// <param name="directoriesCount">The number of directories to step out of.</param>
-        /// <returns></returns>
-        private string stepOut(int directoriesCount)
-        {
-            string currentPath = AppDomain.CurrentDomain.BaseDirectory;
-
-            for (int i = 0; i < directoriesCount; i++)
-            {
-                int mark = currentPath.LastIndexOf('\\',currentPath.Length - 2);
-                char[] newPath = currentPath.Take(mark).ToArray();
-                StringBuilder s = new StringBuilder();
-
-                for (int j = 0; j < newPath.Length; j++)
-                {
-                    s.Append(newPath[j]);
-                }
-
-                s.Append('\\');
-                currentPath = s.ToString();
-            }
-            return currentPath;
-        }
+        
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {            
             Process consoleProcess = new Process();
-            string processPath = System.IO.Path.Combine(stepOut(3), @"Try1\bin\Debug\Try1.exe");
+            string processPath = System.IO.Path.Combine(Misc.stepOut(3), @"Try1\bin\Debug\Try1.exe");
             consoleProcess.StartInfo.FileName = processPath;
-            Process.Start(processPath);
+            
+            consoleProcess.Start();
+            Process.GetCurrentProcess().CloseMainWindow();
         }
     }
 }
--- SpaceGame0.1/Try1/Program.cs
+++ SpaceGame0.1/Try1/Program.cs
@@ -3,16 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Try1
 {
-    //Commit1, vnci
+    //Demonstrating window switch
     class Program
     {
+
+
+        static void AskWhatNext()
+        {
+            Console.WriteLine("\nSwitch back to the window? (Y/N)");
+
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.Y:
+                    {
+                        string basePath = SampleInterface.Misc.stepOut(3);
+                        string fullPath = System.IO.Path.Combine(basePath, @"SampleInterface\bin\Debug\SampleInterface.exe");
+                        Process.Start(fullPath);
+                        Process.GetCurrentProcess().CloseMainWindow();
+
+                        break;
+                    }
+                case ConsoleKey.N:
+                    Process.GetCurrentProcess().CloseMainWindow();
+                    break;
+                default:
+                    break;
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Wow!");
-            Console.ReadLine();
+            AskWhatNext();
+            
+            
         }
     }
 }
