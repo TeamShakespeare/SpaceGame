0934c7b1a66c537aff0db94518e5b70944850fc9
vnci <vbahovski@gmail.com>
2014-02-09 19:09:40 +0200

    Проба за commit

---
+++ SpaceGame0.1/Try1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try1
+{
+    //Commit1, vnci
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
