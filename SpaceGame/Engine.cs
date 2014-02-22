bddda2c6e67b29b940e7e50ace7aef5ccd5e6760
vnci <vbahovski@gmail.com>
2014-02-22 14:06:56 +0200

    Added  to diagram

---
+++ SpaceGame/Engine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceGame
+{
+    public class Engine
+    {
+        public void CheckCollision()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MoveBots()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
